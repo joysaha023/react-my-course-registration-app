5db4734fe717e20576ede2e217065b7662bb3a9b
Joy <github.joysaha@gmail.com>
2024-03-14 17:01:54 +0600

    final and last commit

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -4,13 +4,14 @@ import { totalCredits } from '../courses/Courses';
 const Cart = ({carts}) => {
     const credits =  carts.reduce((p, c) => p + c.credit, 0);
     return (
-        <div className='bg-gray-100'>
-            <h1 className=''>credit Hour Ranking: {totalCredits - credits} hr</h1>
+        <div className='bg-gray-100 rounded-lg'>
+            <h1 className='text-xl text-center font-semibold pt-5'>Credit Hour Ranking: {totalCredits - credits} hr</h1>
             <div className='divider'></div>
-            <div>
-                <h4>Course Name</h4>
-                <div>
-                    <ul>
+            <div className='text-center'>
+                <h4 className=''>Course Name</h4>
+                <hr className='border-2' />
+                <div className='mt-1'>
+                    <ul className='text-base'>
                         {
                             carts.map((c) => (<li key={c.id}>{c.name}</li>))
                         }
@@ -18,9 +19,9 @@ const Cart = ({carts}) => {
                 </div>
             </div>
             <div className='divider'></div>
-            <div>Total credit Hour: {credits}</div>
+            <div className='text-center'>Total credit Hour: {credits}</div>
             <div className='divider'></div>
-            <div>Total Price: {carts.reduce((p, c) => p + c.price, 0)} USD</div>
+            <div className='text-center pb-6'>Total Price: {carts.reduce((p, c) => p + c.price, 0)} USD</div>
         </div>
     );
 };
